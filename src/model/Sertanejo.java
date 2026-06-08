bc185fe58e5126d2ae88dd2c3c2902147bed32a4
kdacecil <kdamaria04@gmail.com>
2026-06-08 17:45:26 -0300

    sertanejo

--- src/model/Sertanejo.java
+++ src/model/Sertanejo.java
@@ -3,4 +3,8 @@ package model;
 public class Sertanejo {
     private String dupla;
 
+    public Sertanejo(String titulo, String artista, int duracaoSegundos, String dupla){
+        super(titulo, artista, duracaoSegundos);
+        this.dupla=dupla;
+    }
 }
