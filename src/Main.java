23133d4d1388bdb9361a7877daff481e80cfa318
kdacecil <kdamaria04@gmail.com>
2026-06-09 14:22:03 -0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
 import service.GerenciadorPlaylist;
-import model,*;
-import java util.imputMismachException;
+import model.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+
+
+public class Main {
+    public static void  main (String args[]){
+     GerenciadorPlaylist gerenciasor = new GerenciadorPlaylist();
+
+     Scanner scanner= new Scanner (System.in);
+    }
+}
