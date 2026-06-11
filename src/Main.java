3b7aba11caa1fb445cbda396de616d2df8c37d19
kdacecil <kdamaria04@gmail.com>
2026-06-11 10:40:36 -0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,33 @@
-import service.GerenciadorPlaylist;
-import model.*;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+    import service.GerenciadorPlaylist;
+    import model.*;
+    import java.util.InputMismatchException;
+    import java.util.Scanner;
 
 
 
-public class Main {
-    public static void  main (String args[]){
-     GerenciadorPlaylist gerenciasor = new GerenciadorPlaylist();
+    public class Main {
+        public static void  main (String args[]){
+         GerenciadorPlaylist gerenciasor = new GerenciadorPlaylist();
 
-     Scanner scanner= new Scanner (System.in);
+         Scanner scanner= new Scanner (System.in);
+        }
+             gerenciador.addMusica(new(Rock, "Bohemian Rapshody, Queen," 355, "Rock progressivo" ));
+            gerenciador.addMusica(new (Rock "Black in back, AC/DC", 255, " Rock Classico" ));
+
+            gerenciador.addMusica(new Pop("As It Was", "Harry Styles", 167, 174));
+            gerenciador.addMusica(new Pop("Blinding Lights", "The Weeknd", 200, 171));
+
+            gerenciador.addMusica(new MPB("Aquarela", "Toquinho", 252, "MPB Clássica"));
+            gerenciador.addMusica(new MPB("Chega de Saudade", "Tom Jobim", 121, "Bossa Nova"));
+
+            gerenciador.addMusica(new Eletronica("Animals", "Martin Garrix", 304, "Sintetizador Digital"));
+            gerenciador.addMusica(new Sertanejo("Evidências", "Chitãozinho & Xororó", 295, "Sertanejo Antigo"));
+            gerenciador.addMusica(new Jazz("So What", "Miles Davis", 562, "Trompete"));
+            gerenciador.addMusica(new Rap("Lose Yourself", "Eminem", 326, 95));
+
+
+         int opcao=0;
+
+
+
     }
- gerenciador.addMusica(new(Rock, "Bohemian Rapshody, Queen," 355, "Rock progressivo" ));
-gerenciador.addMusica(new (Rock "Black in back, AC/DC", 255, " Rock Classico" ));
-
-
-
-}
