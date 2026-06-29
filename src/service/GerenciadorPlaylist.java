df4416c6a7cc99a73509c40f16c7b0e8e82374c9
kdacecil <kdamaria04@gmail.com>
2026-06-28 21:30:58 -0300

    gerenciador

--- src/service/GerenciadorPlaylist.java
+++ src/service/GerenciadorPlaylist.java
@@ -1,4 +1,5 @@
 package service;
+import exception.CatalogoVazio;
 import model.Musica;
 import modelEstiloNaoEncontradoException;
 import model.QuantidadeIndispoNivelException;
@@ -9,6 +10,25 @@ import java.util.List;
 import java.util.Collection;
 
 public class GerenciadorPlaylist {
+    private List<Musica> catalogo;
+
+    public GerenciadorPlaylist() {
+        this.catalogo = new ArrayList<>();
+    }
 
 
+    public void adicionarMusica(Musica m) {
+        if (m != null) {
+            this.catalogo.add(m);
+        }
+    }
+
+public void gerarPlaylist(String estilo,int qtd){
+        if(!this.catalogo.isEmpty()){
+            throw new CatalogoVazioException("Impossivel gerar playlist: o catálogo está completamente vazio");
+
+        }
+
+        List<Musica> musicasFiltradas = new ArrayList<>();
+}
 }
