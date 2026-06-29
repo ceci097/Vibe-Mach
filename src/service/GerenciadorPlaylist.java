1d014cac4f989446cbdc7eb406783ac1d16d051e
kdacecil <kdamaria04@gmail.com>
2026-06-28 21:08:37 -0300

    gerenciador

--- src/service/GerenciadorPlaylist.java
+++ src/service/GerenciadorPlaylist.java
@@ -1,5 +1,31 @@
 package service;
+import model.Musica;
+import modelEstiloNaoEncontradoException;
+import model.QuantidadeIndispoNivelException;
+import model.CatalogoVazio;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collection;
 
 public class GerenciadorPlaylist {
+    private List<Musica> catalogo;
+
+    public GerenciadorPlaylist(){
+        this.catalogo = new ArrayList<>();
+
+    }
+
+    public void adicionarMusica(Musica m){
+        if(m != null){
+            this,catalogo.add(m);
+        }
+    }
+
+    public void ListarTodas(){
+        if(this.catalogo.isEmpty()){
+
+        }
+    }
 
 }
